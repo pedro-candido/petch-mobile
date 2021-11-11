8a42d41b55eab773a0cead3fda957288462e523a
pedro-candido <pedroferreirac@hotmail.com>
2021-11-11 14:27:28 -0300

    feat(button): login page started, button created
    top side of page made, with background color defined, input, button and text styled
    
    #4 #6

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -5,4 +5,15 @@ export const StyledButton = styled.TouchableOpacity`
   border-radius: 6px;
   border: 2px solid #fff;
   width: 100%;
+  height: 46px;
+  align-items: center;
+  justify-content: center;
+  margin: 5% 0;
+`;
+
+export const StyledText = styled.Text`
+  color: #fff;
+  display: flex;
+  font-weight: 600;
+  font-size: 18px;
 `;
